b8703a3b95d9f6c97616c9f8e2bff79ceaea4e71
Hugo Huart <hugo.huart@heig-vd.ch>
2023-05-26 16:38:24 +0200

    kruskal: done

--- src/main/java/gre/lab2/groupe16/KruskalMazeGenerator.java
+++ src/main/java/gre/lab2/groupe16/KruskalMazeGenerator.java
@@ -1,18 +1,40 @@
 package gre.lab2.groupe16;
 
+import gre.lab2.graph.Edge;
+import gre.lab2.graph.VertexLabelling;
 import gre.lab2.gui.MazeBuilder;
 import gre.lab2.gui.MazeGenerator;
+import gre.lab2.gui.Progression;
 
+import java.util.ArrayList;
+import java.util.Collections;
+
+/**
+ * Générateur de labyrinthes avec implémentation itérative
+ * de l'algorithme de Kruskal à l'aide d'une structure Union-Find.
+ */
 public final class KruskalMazeGenerator implements MazeGenerator {
+
   @Override
   public void generate(MazeBuilder builder, int from) {
-    // Affichage : après union, marquer les deux extrémités de l'arête traitée comme PROCESSED puis supprimer le mur
+    UnionFind scc = new UnionFind(builder.topology().nbVertices());
+
+    ArrayList<Edge> edges = new ArrayList<>(builder.topology().edges());
+    Collections.shuffle(edges);
+
+    for (Edge edge : edges) {
+      if (scc.union(edge.u(), edge.v())) {
+        // Les sommets ne font pas encore partie de la même composante connexe
+        builder.removeWall(edge.u(), edge.v());
+        builder.progressions().setLabel(edge.u(), Progression.PROCESSED);
+        builder.progressions().setLabel(edge.v(), Progression.PROCESSED);
+      }
+    }
   }
 }
 
 // TODO
-//  - Renommage du package ;
-//  - Implémentation des classes KruskalMazeGenerator, UnionFind et BfsSolver ;
-//  - Documentation abondante des trois classes comprenant :
-//    - la javadoc, avec auteurs et description des implémentations ;
-//    - des commentaires sur les différentes parties de vos algorithmes.
+//  - Implémentation des classes KruskalMazeGenerator;
+//  - Documentation abondanste des trois classes comprenant :
+//    - la javadoc, avec austeurs et description des implémentations ;
+//    - des commentaires susr les différentes parties de vos algorithmes.
