cf61682cce8d3d38fe5b88aa3e2e01415d8b8ac9
Hugo Huart <hugo.huart@heig-vd.ch>
2023-05-26 16:38:47 +0200

    bfs: wip

--- src/main/java/gre/lab2/groupe16/BfsSolver.java
+++ src/main/java/gre/lab2/groupe16/BfsSolver.java
@@ -6,9 +6,32 @@ import gre.lab2.gui.MazeSolver;
 
 import java.util.*;
 
+/**
+ * Solver de labyrinthe avec implémentation itérative d'un parcours en largeur.
+ */
 public final class BfsSolver implements MazeSolver {
+
   @Override
   public List<Integer> solve(Graph graph, int source, int destination, VertexLabelling<Integer> treatments) {
-    return Collections.emptyList();
+    LinkedList<Integer> queue = new LinkedList<>(graph.neighbors(source));
+    ArrayList<Integer> path = new ArrayList<>(graph.nbVertices());
+
+    while (!queue.isEmpty()) {
+      int u = queue.pop();
+
+      for (int v : graph.neighbors(u)) {
+        // Le sommet non-découvert
+        if (treatments.getLabel(v) == 0) {
+          // Le sommet est maintenant découvert
+          treatments.setLabel(v, 1);
+          queue.add(v);
+        }
+      }
+
+      // Le sommet est traité
+      treatments.setLabel(u, 2);
+    }
+
+    return path;
   }
 }
