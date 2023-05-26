203c5002a14f82c1a5638b2f9ab99a4e4e70ba4d
Hugo Huart <hugo.huart@heig-vd.ch>
2023-05-26 16:38:05 +0200

    unionfind: done

--- src/main/java/gre/lab2/groupe16/UnionFind.java
+++ src/main/java/gre/lab2/groupe16/UnionFind.java
@@ -1,15 +1,59 @@
 package gre.lab2.groupe16;
 
+/**
+ * Implémentation d'une structure Union-Find avec représentation par des arbres.
+ * Utilise la méthode de <i>path-splitting</i> pour les opérations <i>find</i>.
+ * Utilise la méthode d'union par rangs pour les opérations <i>union</i>.
+ */
 final class UnionFind {
+  private int[] parents;
+  private int[] ranks;
+
   public UnionFind(int size) {
+    parents = new int[size];
+    ranks = new int[size];
+
+    for (int i = 0; i < size; ++i) {
+      parents[i] = i;
+      ranks[i] = 0;
+    }
   }
 
+  /**
+   *
+   * @param v Un sommet
+   * @return La racine de l'arbre du sommet .
+   */
   public int find(int v) {
-    return 0;
+    while (v != parents[v]) {
+      v = parents[v];
+      parents[v] = parents[parents[v]];
+    }
+    return v;
   }
 
   public boolean union(int u, int v) {
-    // Indication : retourne false si les deux sommets font partie de la même composante connexe
-    return false;
+    u = find(u);
+    v = find(v);
+
+    if (u == v) {
+      // Les deux sommets font déjà partie de la même composante connexe
+      return false;
+    }
+
+    // Réassignation afin que u et v correspondent aux arbres de rangs max et min respectivement
+    if (ranks[u] < ranks[v]) {
+      int temp = u;
+      u = v;
+      v = temp;
+    }
+
+    parents[v] = u;
+
+    if (ranks[u] == ranks[v]) {
+      ranks[u] += 1;
+    }
+
+    return true;
   }
 }
