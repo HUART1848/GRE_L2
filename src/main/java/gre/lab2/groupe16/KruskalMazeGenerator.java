75d472f7f60ece6138a111035ba8f7dccabca149
Hugo Huart <hugo.huart@heig-vd.ch>
2023-05-28 20:08:03 +0200

    kruskal: finito

--- src/main/java/gre/lab2/groupe16/KruskalMazeGenerator.java
+++ src/main/java/gre/lab2/groupe16/KruskalMazeGenerator.java
@@ -1,7 +1,6 @@
 package gre.lab2.groupe16;
 
 import gre.lab2.graph.Edge;
-import gre.lab2.graph.VertexLabelling;
 import gre.lab2.gui.MazeBuilder;
 import gre.lab2.gui.MazeGenerator;
 import gre.lab2.gui.Progression;
@@ -12,11 +11,13 @@ import java.util.Collections;
 /**
  * Générateur de labyrinthes avec implémentation itérative
  * de l'algorithme de Kruskal à l'aide d'une structure Union-Find.
+ * @author Farouk Ferchichi & Hugo Huart
  */
 public final class KruskalMazeGenerator implements MazeGenerator {
 
   @Override
   public void generate(MazeBuilder builder, int from) {
+    // Structure Union-Find pour le calcul des composantes fortement connexes
     UnionFind scc = new UnionFind(builder.topology().nbVertices());
 
     ArrayList<Edge> edges = new ArrayList<>(builder.topology().edges());
@@ -25,16 +26,10 @@ public final class KruskalMazeGenerator implements MazeGenerator {
     for (Edge edge : edges) {
       if (scc.union(edge.u(), edge.v())) {
         // Les sommets ne font pas encore partie de la même composante connexe
-        builder.removeWall(edge.u(), edge.v());
         builder.progressions().setLabel(edge.u(), Progression.PROCESSED);
         builder.progressions().setLabel(edge.v(), Progression.PROCESSED);
+        builder.removeWall(edge.u(), edge.v());
       }
     }
   }
 }
-
-// TODO
-//  - Implémentation des classes KruskalMazeGenerator;
-//  - Documentation abondanste des trois classes comprenant :
-//    - la javadoc, avec austeurs et description des implémentations ;
-//    - des commentaires susr les différentes parties de vos algorithmes.
