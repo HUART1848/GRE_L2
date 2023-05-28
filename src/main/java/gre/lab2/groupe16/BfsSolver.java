e2a707ffebc7f91d167de7eeb779688be181a2a4
Hugo Huart <hugo.huart@heig-vd.ch>
2023-05-28 20:07:49 +0200

    bfs: finito

--- src/main/java/gre/lab2/groupe16/BfsSolver.java
+++ src/main/java/gre/lab2/groupe16/BfsSolver.java
@@ -7,29 +7,54 @@ import gre.lab2.gui.MazeSolver;
 import java.util.*;
 
 /**
- * Solver de labyrinthe avec implémentation itérative d'un parcours en largeur.
+ * Solver de labyrinthe avec implémentation itérative d'un parcours en largeur (BFS).
+ * @author Farouk Ferchichi & Hugo Huart
  */
 public final class BfsSolver implements MazeSolver {
 
   @Override
   public List<Integer> solve(Graph graph, int source, int destination, VertexLabelling<Integer> treatments) {
-    LinkedList<Integer> queue = new LinkedList<>(graph.neighbors(source));
-    ArrayList<Integer> path = new ArrayList<>(graph.nbVertices());
+    // Cas trivial
+    if (source == destination) {
+      treatments.setLabel(source, 2);
+      return Arrays.asList(source, source);
+    }
 
-    while (!queue.isEmpty()) {
+    LinkedList<Integer> queue = new LinkedList<>();
+    int[] predecessors = new int[graph.nbVertices()];
+
+    queue.add(source);
+    treatments.setLabel(source, 1);
+
+    boolean searching = true;
+    while (searching && !queue.isEmpty()) {
       int u = queue.pop();
 
       for (int v : graph.neighbors(u)) {
-        // Le sommet non-découvert
         if (treatments.getLabel(v) == 0) {
-          // Le sommet est maintenant découvert
+          predecessors[v] = u;
+
+          // Arrêt anticipé de l'algorithme si la destination est découverte
+          if (v == destination) {
+            treatments.setLabel(v, 2);
+            searching = false;
+            break;
+          }
+
           treatments.setLabel(v, 1);
           queue.add(v);
         }
       }
 
-      // Le sommet est traité
       treatments.setLabel(u, 2);
+    }
+
+    // Reconstruction du plus court chemin à l'aide des prédécesseurs
+    LinkedList<Integer> path = new LinkedList<>();
+    path.add(destination);
+    while (destination != source) {
+      destination = predecessors[destination];
+      path.addFirst(destination);
     }
 
     return path;
